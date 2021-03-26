1122c99d1cb8f2020f45dcc29b402d074b342abb
codershuvo <nisho.mushfiq2020@gmail.com>
2021-03-27 02:57:29 +0600

    final ema john

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,24 +2,26 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import { Link } from "react-router-dom";
 
 export default function Product(props) {
-    const {img, name, seller, price, stock} = props.product;
+    const {img, name, seller, price, stock, key} = props.product;
+
     return (
         <div className='product'>
             <div>
                 <img src={img} alt=""/>
             </div>
             <div>
-                <h4 className='product-name'>{name}</h4>
+                <h4 className='product-name'> <Link to={"/product/"+key}>{name}</Link> </h4>
                 <br/>
                 <p><small>by: {seller}</small></p>
                 <p>$ {price}</p>
                 <p><small>Ony {stock} left in stock - Order soon</small></p>
-                <button className='main-button'
+                {props.showAddToCart && <button className='main-button'
                     onClick={() => props.handleAddProduct(props.product)}
                 >
-                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart</button>
+                <FontAwesomeIcon icon={faCartPlus} /> Add to Cart</button>}
             </div>
         </div>
     )
